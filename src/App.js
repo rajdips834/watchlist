6c7f49fb92d4202701b7990886a04b974d1354a2
Rajdip Sinha <rajdips834@gmail.com>
2024-07-23 21:00:17 +0530

    Removed boilerplate code from appjs

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import MyState from "./context/MyState";
+import { useState } from "react";
 
 function App() {
+  const [movies, setMovies] = useState([]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <MyState>
+      <div className="App"></div>
+    </MyState>
   );
 }
 
