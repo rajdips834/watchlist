490dd812ec99378749588865a9c14a124fab7aa3
Rajdip Sinha <rajdips834@gmail.com>
2024-07-23 23:09:30 +0530

    Card added

---
+++ src/components/card/Card.js
@@ -0,0 +1,21 @@
+import React from "react";
+import styles from "./card.module.css"; // Ensure this is the correct path
+
+export default function Card({ image, title, year, rating, width, height }) {
+  return (
+    <div
+      className={styles.card}
+      style={{
+        width: width ? width : "16rem",
+        height: height ? height : "36rem",
+      }}
+    >
+      <img src={image} className={styles.cardImgTop} alt={title} />
+      <div className={styles.cardbody}>
+        <h5 className={styles.cardTitle}>{title}</h5>
+        <p className={styles.cardText}>Year: {year}</p>
+        <p className={styles.cardText}>Rating: {rating}</p>
+      </div>
+    </div>
+  );
+}
