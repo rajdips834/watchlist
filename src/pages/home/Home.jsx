63fee47ec27a29986980e0b1a0b4047121cd1553
Rajdip Sinha <rajdips834@gmail.com>
2024-07-27 03:37:31 +0530

    Code cleanup in home page

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,19 +1,14 @@
 import React, { useState, useContext, useEffect } from "react";
-import movies from "../../movies.json";
 import CardList from "../../lists/cardList/CardList";
-import useWindowSize from "../../hooks/useWindowSize";
 import myContext from "../../context/MyContext";
 import styles from "./Home.module.css";
 import Search from "../../components/search/Search";
 import Header from "../../components/header/Header";
 import WatchList from "../../components/watchList/WatchList";
 import AddToPlaylist from "../../components/modal/AddToPlaylist";
-import { FaBars } from "react-icons/fa";
-import Loader from "../../components/loader/Loader";
 export default function Home() {
   const context = useContext(myContext);
-  const { searchKey, movies, results } = context;
-  const { width } = useWindowSize();
+  const { movies } = context;
 
   return (
     <>
